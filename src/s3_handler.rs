f74f6c85cb62029418f0cb1a2b0a54cd11f95a86
Alex Nederlof <alex@nederlof.com>
2023-12-28 14:44:56 +0200

    Copy over all header values

--- src/s3_handler.rs
+++ src/s3_handler.rs
@@ -1,17 +1,13 @@
-use std::str::FromStr;
+use std::time::{Duration, SystemTime};
 
 use crate::app_config::AppConfig;
 use actix_web::{
-    http::header::{self, EntityTag, HttpDate},
+    http::header::{self, HttpDate},
     web::{self, Bytes},
     HttpRequest, Responder,
 };
 use aws_sdk_s3::error::SdkError;
-use aws_smithy_types_convert::date_time::DateTimeExt;
-use chrono::Utc;
 use log::{error, info};
-
-use futures::stream;
 
 pub async fn s3(
     req: HttpRequest,
@@ -40,34 +36,8 @@ pub async fn s3(
     match resp {
         Ok(resp) => {
             let mut builder: actix_web::HttpResponseBuilder = actix_web::HttpResponse::Ok();
-            builder.content_type(resp.content_type().unwrap());
 
-            for (header_name, header_value) in resp.headers().iter() {
-                if header_name == "Cache-Control"
-                    || header_name == "Expires"
-                    || header_name == "Last-Modified"
-                    || header_name == "ETag"
-                {
-                    builder.append_header((header_name, header_value.clone()));
-                }
-            }
-
-            if let Some(etag) = resp.e_tag() {
-                let etag = etag.trim_matches('"').to_string();
-                builder.append_header(header::ETag(EntityTag::new_strong(etag)));
-            }
-            if let Some(expires) = resp.expires() {
-                let chrono_date_time: chrono::DateTime<Utc> = expires.to_chrono_utc().unwrap();
-
-                match HttpDate::from_str(chrono_date_time.to_rfc2822().as_str()) {
-                    Ok(http_date) => {
-                        builder.append_header(header::Expires(http_date));
-                    }
-                    Err(e) => {
-                        error!("Error parsing date: {:?}", e);
-                    }
-                }
-            }
+            copy_headers(&resp, &mut builder);
 
             let body = resp.body;
 
@@ -104,5 +74,54 @@ pub async fn s3(
                 actix_web::HttpResponse::NotFound().body("Not found")
             }
         },
+    }
+}
+
+fn copy_headers(
+    resp: &aws_sdk_s3::operation::get_object::GetObjectOutput,
+    builder: &mut actix_web::HttpResponseBuilder,
+) {
+    if let Some(etag) = resp.e_tag() {
+        builder.append_header(("ETag", etag));
+    }
+    if let Some(expires) = resp.expires() {
+        let sys_time = SystemTime::UNIX_EPOCH
+            .checked_add(Duration::from_secs_f64(expires.as_secs_f64()))
+            .unwrap();
+        builder.append_header(header::Expires(HttpDate::from(sys_time)));
+    }
+    if let Some(last_modified) = resp.last_modified() {
+        let sys_time = SystemTime::UNIX_EPOCH
+            .checked_add(Duration::from_secs_f64(last_modified.as_secs_f64()))
+            .unwrap();
+        builder.append_header(header::LastModified(HttpDate::from(sys_time)));
+    }
+
+    if let Some(accept_ranges) = resp.accept_ranges() {
+        builder.append_header(("Accept-Ranges", accept_ranges));
+    }
+
+    if let Some(language) = resp.content_language() {
+        builder.append_header(("Content-Language", language));
+    }
+
+    if let Some(content_disposition) = resp.content_disposition() {
+        builder.append_header(("Content-Disposition", content_disposition));
+    }
+
+    if let Some(cache_control) = resp.cache_control() {
+        builder.append_header(("Cache-Control", cache_control));
+    }
+
+    if let Some(content_encoding) = resp.content_encoding() {
+        builder.append_header(("Content-Encoding", content_encoding));
+    }
+
+    if let Some(content_length) = resp.content_length() {
+        builder.append_header(header::ContentLength(content_length as usize));
+    }
+
+    if let Some(content_type) = resp.content_type() {
+        builder.append_header(("Content-Type", content_type));
     }
 }
